fc563d0fc3b77e3f0b4e72385bf0f7cf3878da7e
Zammy <simonez2001@gmail.com>
2023-07-16 16:59:32 +0200

    .

--- app/src/main/java/com/example/replica/MainActivity.kt
+++ app/src/main/java/com/example/replica/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.example.replica
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.widget.Toolbar
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var toolbar: Toolbar
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val bottone = findViewById<Button>(R.id.button)
+        bottone.setOnClickListener{
+            var intent = Intent(this, MainActivity2 :: class.java)
+            startActivity(intent)
+        }
     }
 }
